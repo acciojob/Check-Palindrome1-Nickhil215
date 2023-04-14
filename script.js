049a917799a6ccca4594c836d75a1d87612596f7
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2023-04-14 17:03:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // complete the given function
 
+
 function palindrome(str){
+	str=str.toLowerCase();
+	
+	let s='';
+	for(let i =0;i<str.length;i++){
+		let temp=str[i];
+		if(isCharacterALetter(temp)){
+			s+=temp;
+		}
+	}
+
+    console.log(s);
+
+let i=0;
+	let j=s.length-1;
+	while(i<=j){
+		if(s[i]!=s[j]){
+			return false;
+		}
+        i++;
+        j--;
+	}
+
+	return true;
 
 }
+function isCharacterALetter(char) {
+    return (/[a-zA-Z]/).test(char)
+  }
 module.exports = palindrome
